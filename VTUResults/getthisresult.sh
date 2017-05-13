bc837eee98bfadc74b731e0f5a688f6c3ea413b1
Ravikiran <kienme@gmail.com>
2017-05-14 01:44:36 +0530

    Script to download one result

---
+++ VTUResults/getthisresult.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+USN_BASE="1mv14cs"
+URL="http://results.vtu.ac.in/results/result_page.php?usn="
+OUT_FOLDER="Results"
+
+download() # $1=from $2=to $3=usn
+{
+	usn=$1
+	flag=0
+	while [ $flag -eq 0 ];	do
+		echo "Downloading $usn"
+		curl --silent --max-time 10 "$URL$usn" > "$OUT_FOLDER/$usn.html"
+		if [ $? -eq 0 ]; then
+			((flag++))
+		fi
+	done
+}
+
+mkdir $OUT_FOLDER
+
+download "$1"
