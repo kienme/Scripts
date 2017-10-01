b42de1d4471edd9267d991efb926907a590cac2f
RaghavaDhanya <raghava.dhanya@gmail.com>
2017-10-01 16:50:42 +0530

    formatting usn in getallresults.sh!

--- VTUResults/getallresults.sh
+++ VTUResults/getallresults.sh
@@ -4,13 +4,13 @@ USN_BASE="1mv14cs"
 URL="http://results.vtu.ac.in/results17/result_page.php?usn="
 OUT_FOLDER="Results"
 
-download() # $1=from $2=to $3=usn
+download() # $1=from $2=to
 {
-	usn=$3
 	counter=$1
 	while [ $counter -lt $2 ];	do
-		echo "Downloading $usn$counter"
-		curl --silent --max-time 10 "$URL$usn$counter" > "$OUT_FOLDER/$usn$counter.html"
+		usn=`printf "$USN_BASE%03d" $counter`
+		echo "Downloading $usn"
+		curl --silent --max-time 10 "$URL$usn" > "$OUT_FOLDER/$usn.html"
 		if [ $? -eq 0 ]; then
 			((counter++))
 		fi
@@ -19,6 +19,4 @@ download() # $1=from $2=to $3=usn
 
 mkdir $OUT_FOLDER
 
-download 1   10  "$USN_BASE""00"
-download 10  100 "$USN_BASE""0"
-download 100 137 "$USN_BASE"
+download 1 137
